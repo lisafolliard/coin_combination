1e41cf1f687029dce1b0496ba6ebf9a4e59c23b1
Lisa Folliard & Toma Apostolov <student@epicodus.com>
2015-08-06 15:23:56 -0700

    rewrote and passed first spec

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,6 +1,50 @@
 class Fixnum
-
   define_method(:coin_combination) do
-    quarters_check = self./(33)
+
+    amount = self
+    coins = []
+
+    # remainder = self
+    # quarters = 0
+    # until remainder < 25
+    #   quarters = quarters + 1
+    #   remainder = remainder - 25
+    # end
+
+
+
+    # quarters
+    quarters_check = amount./(25)
+    quarters_result = quarters_check.to_s().concat(" quarters")
+    coins.push(quarters_result)
+    amount = amount - quarters_check
+
+    # dimes
+
+
+
+
+      # dimes_check = amount./(10)
+      # dimes_check = dimes_check.to_s().concat(" dimes")
+      # coins.push(dimes_check)
+
+
+      #  amount = amount.%(quarters_check)
+
+
+
+
+    #
+    #
+    #
+    #
+    # elsif
+    #   dimes_check = self./(10)
+    #   dimes = dimes_check.%(10)
+    #   dimes_check = dimes_check.to_s().concat(" dimes")
+    #   coins.push(quarters_check)
+    # end
+
+    coins.join(", ")
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -2,8 +2,13 @@ require('rspec')
 require('coin_combination')
 
   describe('Fixnum#coin_combination') do
-    it("takes an amount of cents and shows how many quarters are needed as part of the change") do
-      expect((33).coin_combination()).to(eq(1))
-
+    it("takes a quarter and displays how many quarters
+    are needed as part of the change") do
+      expect((25).coin_combination()).to(eq("1 quarters"))
     end
+
+      # it("takes a dime and displays how many dimes are needed as part of
+      # the change") do
+      #   expect((10).coin_combination()).to(eq("1 dimes"))
+      # end
   end
