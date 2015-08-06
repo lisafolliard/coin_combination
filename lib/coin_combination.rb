b5991f0cbff742a73c8a8bdcaacd6af4eb62022c
Lisa Folliard & Toma Apostolov <student@epicodus.com>
2015-08-06 16:05:47 -0700

    cleaned up spacing and indents

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -5,52 +5,18 @@ class Fixnum
     remainder = amount
 
     # quarters
-  if remainder <=99 && remainder >=25
-    quarters_check = amount./(25)
-    quarters_result = quarters_check.to_s().concat(" quarters")
-    coins.push(quarters_result)
+    if remainder <=99 && remainder >=25
+      quarters_check = amount./(25)
+      quarters_result = quarters_check.to_s().concat(" quarters")
+      coins.push(quarters_result)
+    end
 
-end
     # dimes
-  if remainder <= 10 && remainder >=5
-
-
-    dimes_check = remainder./(10)
-    dimes_result = dimes_check.to_s().concat(" dimes")
-    coins.push(dimes_result)
-
-end
-
+    if remainder <=10 && remainder >=5
+      dimes_check = remainder./(10)
+      dimes_result = dimes_check.to_s().concat(" dimes")
+      coins.push(dimes_result)
+    end
     coins.join(", ")
   end
 end
-
-
-
-
-
-      # dimes_check = amount./(10)
-      # dimes_check = dimes_check.to_s().concat(" dimes")
-      # coins.push(dimes_check)
-
-
-      #  amount = amount.%(quarters_check)
-
-
-# remainder = self
-# quarters = 0
-# until remainder < 25
-#   quarters = quarters + 1
-#   remainder = remainder - 25
-# end
-
-    #
-    #
-    #
-    #
-    # elsif
-    #   dimes_check = self./(10)
-    #   dimes = dimes_check.%(10)
-    #   dimes_check = dimes_check.to_s().concat(" dimes")
-    #   coins.push(quarters_check)
-    # end
